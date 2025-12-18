f804d5a5486ed04263fc2ba50355eb2d5094ba36
Bogdan Sokolov <sobogd@gmail.com>
2025-12-18 04:14:00 +0100

    stripe fix

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -13,7 +13,7 @@ export const stripe = new Proxy({} as Stripe, {
         throw new Error("STRIPE_SECRET_KEY is not configured");
       }
       stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: "2025-11-17.clover",
+        apiVersion: "2025-12-15.clover",
         typescript: true,
       });
     }
